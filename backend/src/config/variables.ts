df9110d4793aa7a982aea566e472c75fd2911642
m0hanad1 <mohned.dabsha@gmail.com>
2024-11-02 22:44:25 +0200

    Fixed default JWT value

--- backend/src/config/variables.ts
+++ backend/src/config/variables.ts
@@ -4,4 +4,4 @@ config();
 
 export const DBURL = process.env.DBURL || "";
 export const PORT = process.env.PORT || 3000;
-export const JWT_SECRET = process.env.JWT_SECRET || 3000;
+export const JWT_SECRET = process.env.JWT_SECRET;
